08c99e5892f3858bd7e225c050e2ec6f81e54201
Anton Kanopkin <39925338+kanstudio@users.noreply.github.com>
2018-10-21 21:22:17 +0200

    Update snippet.docinfo.php
    1. The &limit parameter was added
    2. Fix false value of $modx->getDocument

--- assets/snippets/docinfo/snippet.docinfo.php
+++ assets/snippets/docinfo/snippet.docinfo.php
@@ -3,11 +3,12 @@
  * DocInfo
  *
  * @category  parser
- * @version   0.4
+ * @version   0.5
  * @license     GNU General Public License (GPL), http://www.gnu.org/copyleft/gpl.html
  * @param string $field Значение какого поля необходимо достать
  * @param int $docid ID документа
  * @param int $templid ID шаблона документа
+ * @param int $limit Максимальная глубина вложенности для поиска по шаблону документа
  * @param int $tv является ли поле TV параметром (0 - нет || 1 - да)
  * @param int $render Преобразовывать ли значение TV параметра в соответствии с его визуальным компонентом
  * @return string Значение поля документа или его TV параметра
@@ -19,16 +20,17 @@
 *       [[DocInfo? &docid=`15` &field=`pagetitle`]]
 *       [[DocInfo? &docid=`10` &field=`tvname`]]
 *       [[DocInfo? &docid=`3` &field=`tvname` &render=`1`]]
-*       [[DocInfo? &docid=`3` &templid=`2` &field=`tvname`]]
+*       [[DocInfo? &docid=`3` &templid=`2` &field=`tvname` &limit=`2`]]
 */
 if(!defined('MODX_BASE_PATH')){die('What are you doing? Get out of here!');}
 $default_field = array('type','contentType','pagetitle','longtitle','description','alias','link_attributes','published','pub_date','unpub_date','parent','isfolder','introtext','content','richtext','template','menuindex','searchable','cacheable','createdon','createdby','editedon','editedby','deleted','deletedon','deletedby','publishedon','publishedby','menutitle','donthit','privateweb','privatemgr','content_dispo','hidemenu','alias_visible');
 $docid = (isset($docid) && (int)$docid>0) ? (int)$docid : $modx->documentIdentifier;
-if(isset($templid) && (int)$templid>0) {
+if(isset($templid) && (int)$templid>=0) {
 	$doc['parent'] = $docid;
-	$limit = 20;
+	$limit = (isset($limit) && (int)$limit>=0) ? (int)$limit+1 : 11;
 	do {
 		$doc = $modx->getDocument($doc['parent'], 'id, parent, template');
+		if($doc === false) return '';
 		if($doc['template'] == (int)$templid) $docid = $doc['id'];
 		$limit--;
 	}
@@ -51,4 +53,3 @@ if (in_array($field, $default_field)) {
     }
 }
 return $output;
-?>
