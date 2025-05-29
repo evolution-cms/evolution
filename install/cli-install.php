119147164acabac8d8caccf2d981dd4abbb8e384
Serhii Korneliuk <seiger@meta.ua>
2025-05-29 17:36:45 +0300

    [ADD] Automatic installation via Composer.

--- install/cli-install.php
+++ install/cli-install.php
@@ -538,7 +538,7 @@ class InstallEvo
         $field['username'] = $this->cmsAdmin;
         $managerUser = EvolutionCMS\Models\User::create($field);
         $internalKey = $managerUser->getKey();
-        $role = \EvolutionCMS\Models\UserRole::where('name', 'Administrator')->first()->getKey();
+        $role = (int)\EvolutionCMS\Models\UserRole::where('name', 'Administrator')->first()?->id;
         $field = ['internalKey' => $internalKey, 'email' => $this->cmsAdminEmail, 'role' => $role, 'verified' => 1];
         $managerUser->attributes()->create($field);
         $managerUser->attributes->role = $role;
