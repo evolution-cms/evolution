ea60be977fb6ec1837904bbe29610a6fa9c0d03b
Serhii Korneliuk <seiger@meta.ua>
2025-08-03 19:57:58 +0300

    [UPD] Core up to Laravel Zero 11.

--- core/factory/version.php
+++ core/factory/version.php
@@ -1,6 +1,6 @@
 <?php return [
-    'version' => '3.2.6', // Current version number
-    'release_date' => 'May 25, 2025', // Date of release
+    'version' => '3.2.7', // Current version number
+    'release_date' => 'Aug 03, 2025', // Date of release
     'branch' => 'Evolution CMS', // Codebase name
-    'full_appname' => 'Evolution CMS 3.2.6 (May 25, 2025)'
+    'full_appname' => 'Evolution CMS 3.2.7 (Aug 03, 2025)'
 ];
