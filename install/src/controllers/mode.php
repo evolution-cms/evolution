3335e67864fdc6b096cbbc461252ce4fde6a7042
Artur Kyryliuk <mail@artur.work>
2026-01-15 04:49:31 +0100

    FIX incorrect work with dbs with dash in the name

--- install/src/controllers/mode.php
+++ install/src/controllers/mode.php
@@ -1,31 +1,35 @@
 <?php
 // Determine upgradeability
-$upgradeable = 0;
+$isNew = true;
+$isConnectable = false;
 if (is_file(EVO_CORE_PATH . 'config/database/connections/default.php')) { // Include the file so we can test its validity
     $db_config = include_once EVO_CORE_PATH . 'config/database/connections/default.php';
     // We need to have all connection settings - tho prefix may be empty, so we have to ignore it
     if (isset($db_config['database'])) {
         try {
-            $dbh = new PDO($db_config['driver'] . ':host=' . $db_config['host'] . ';dbname=' . $db_config['database'], $db_config['username'], $db_config['password']);
-            $upgradeable = isset($_POST['installmode']) && $_POST['installmode'] == 'new' ? 0 : 2;
+            $pdoOptions = [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION];
+            $dbh = new PDO($db_config['driver'] . ':host=' . $db_config['host'] . ';dbname='
+                . $db_config['database'], $db_config['username'], $db_config['password'], $pdoOptions);
+            $isConnectable = true;
         } catch (PDOException $e) {
-            $upgradeable = 1;
+            $isConnectable = false;
         }
+        $isNew = isset($_POST['installmode']) && $_POST['installmode'] === 'new';
     } else {
-        $upgradeable = 2;
+        $isNew = true;
     }
 }
 
 $ph['moduleName'] = $moduleName;
-$ph['displayNew'] = ($upgradeable !== 0) ? 'hidden' : '';
-$ph['displayUpg'] = ($upgradeable === 0) ? 'hidden' : '';
+$ph['displayNew'] = !$isNew ? 'hidden' : '';
+$ph['displayUpg'] = $isNew ? 'hidden' : '';
 $ph['displayAdvUpg'] = $ph['displayUpg'];
-$ph['checkedNew'] = !$upgradeable ? 'checked' : '';
-$ph['checkedUpg'] = (isset($_POST['installmode']) && $_POST['installmode'] == 1 || $upgradeable == 1) ? 'checked' : '';
-$ph['checkedAdvUpg'] = (isset($_POST['installmode']) && $_POST['installmode'] == 2 || $upgradeable == 2) ? 'checked' : '';
+$ph['checkedNew'] = $isNew ? 'checked' : '';
+$ph['checkedUpg'] = (!$isNew && $isConnectable) || (isset($_POST['installmode']) && $_POST['installmode'] == 1) ? 'checked' : '';
+$ph['checkedAdvUpg'] = (!$isNew && !$isConnectable) || (isset($_POST['installmode']) && $_POST['installmode'] == 2) ? 'checked' : '';
 $ph['install_language'] = $install_language;
-$ph['disabledUpg'] = ($upgradeable != 1) ? 'disabled' : '';
-$ph['disabledAdvUpg'] = ($upgradeable == 0) ? 'disabled' : '';
+$ph['disabledUpg'] = !$isConnectable ? 'disabled' : '';
+$ph['disabledAdvUpg'] = '';
 $ph['csrf_nonce'] = csrfNonce();
 
 $tpl = file_get_contents(dirname(__DIR__) . '/template/actions/mode.tpl');
