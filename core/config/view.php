284a7bc30099f2a6cd44d2f5b229785a6c4967ab
Serhii Korneliuk <info@seigerit.com>
2026-01-22 03:34:00 +0200

    [REF] Registration Blade directives.

--- core/config/view.php
+++ core/config/view.php
@@ -5,18 +5,22 @@ return [
     ],
     'compiled' => EVO_STORAGE_PATH . 'blade',
     'directive' => [
+        //----------
+        /**
+         * @deprecated
+         * @since 3.5.3
+         *
+         * It's not using anywhere.
+         *
+         * @todo [remove@3.7] Remove in Evolution CMS 3.7
+         */
         'csrf' => [EvolutionCMS\Support\BladeDirective::class, 'csrf'],
-        'makeUrl' => [EvolutionCMS\Support\BladeDirective::class, 'makeUrl'],
-        'evoParser' => [EvolutionCMS\Support\BladeDirective::class, 'evoParser'],
         'evoLang' => [EvolutionCMS\Support\BladeDirective::class, 'evoLang'],
         'evoStyle' => [EvolutionCMS\Support\BladeDirective::class, 'evoStyle'],
         'evoAdminLang' => [EvolutionCMS\Support\BladeDirective::class, 'evoAdminLang'],
         'evoCharset' => [EvolutionCMS\Support\BladeDirective::class, 'evoCharset'],
         'evoAdminThemeUrl' => [EvolutionCMS\Support\BladeDirective::class, 'evoAdminThemeUrl'],
         'evoAdminThemeName' => [EvolutionCMS\Support\BladeDirective::class, 'evoAdminThemeName'],
-        'evoRole' => [EvolutionCMS\Support\BladeDirective::class, 'evoRole'],
-        'evoElseRole' => [EvolutionCMS\Support\BladeDirective::class, 'evoElseRole'],
-        'evoEndRole' => [EvolutionCMS\Support\BladeDirective::class, 'evoEndRole'],
-        'evoConfig' => [EvolutionCMS\Support\BladeDirective::class, 'evoConfig'],
+        //----------
     ]
 ];
