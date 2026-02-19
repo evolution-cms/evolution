2b9a178325dd5c93111a4b5ba16fab520aea8f9b
Serhii Korneliuk <info@seigerit.com>
2026-02-19 23:06:55 +0200

    [UPD] Version.

--- core/factory/version.php
+++ core/factory/version.php
@@ -1,6 +1,6 @@
 <?php return [
     'version' => '3.5.3', // Current version number
-    'release_date' => 'Jan 25, 2026', // Date of release
+    'release_date' => 'Feb 19, 2026', // Date of release
     'branch' => 'Evolution CMS', // Codebase name
-    'full_appname' => 'Evolution CMS 3.5.3 (Jan 25, 2026)'
+    'full_appname' => 'Evolution CMS 3.5.3 (Feb 19, 2026)'
 ];
