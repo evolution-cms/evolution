475eabfd2c9638476a0c2c9d10e62f3b6bd64e8c
Serhii Korneliuk <info@seigerit.com>
2026-04-19 15:46:37 +0300

    [FIX] Changelog.

--- core/factory/version.php
+++ core/factory/version.php
@@ -1,6 +1,6 @@
 <?php return [
     'version' => '3.5.6',
-    'release_date' => 'Apr 7, 2026',
+    'release_date' => 'Apr 19, 2026',
     'branch' => 'Evolution CMS',
-    'full_appname' => 'Evolution CMS 3.5.6 (Apr 7, 2026)'
+    'full_appname' => 'Evolution CMS 3.5.6 (Apr 19, 2026)'
 ];
