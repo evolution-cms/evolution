aa82a36dc749cbf14d8e918969ef1122e3a4f4ad
Serhii Korneliuk <info@seigerit.com>
2026-06-01 16:52:49 +0300

    add(manager): support EvoUI

--- manager/routes.php
+++ manager/routes.php
@@ -1,5 +1,11 @@
-<?php
+<?php use Illuminate\Support\Facades\Route;
 
-use Illuminate\Support\Facades\Route;
+if (class_exists(\EvoUI\Support\LivewireManagerEndpoint::class)) {
+    $evoUiLivewireEndpoint = static function (\Illuminate\Http\Request $request, ?string $path = null) {
+        return app(\EvoUI\Support\LivewireManagerEndpoint::class)($request, $path);
+    };
+
+    Route::match(['GET', 'POST'], 'evo-ui/{path?}', $evoUiLivewireEndpoint)->where('path', '.*');
+}
 
 Route::match(['GET', 'POST'], '/', 'Actions@handleAction');
