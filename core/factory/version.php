0166c6568ca98fec4326052fda513a16b4878a2e
Serhii Korneliuk <info@seigerit.com>
2026-07-21 16:00:46 +0300

    upd(docs): add and normalize 3.5 changelog entries

--- core/factory/version.php
+++ core/factory/version.php
@@ -1,6 +1,6 @@
 <?php return [
-    'version' => '3.5.7',
-    'release_date' => 'Jul 19, 2026',
+    'version' => '3.5.8',
+    'release_date' => 'Oct 3, 2026',
     'branch' => 'Evolution CMS',
-    'full_appname' => 'Evolution CMS 3.5.7 (Jul 19, 2026)'
+    'full_appname' => 'Evolution CMS 3.5.8 (Oct 3, 2026)'
 ];
