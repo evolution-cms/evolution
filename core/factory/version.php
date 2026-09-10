3da2d5590baee6114f7bea6964fd74b4502db193
Serhii Korneliuk <info@seigerit.com>
2026-09-10 15:17:39 +0300

    upd(core): composer start a new version

--- core/factory/version.php
+++ core/factory/version.php
@@ -1,6 +1,6 @@
 <?php return [
-    'version' => '3.5.8',
-    'release_date' => 'Sep 10, 2026',
+    'version' => '3.5.9',
+    'release_date' => 'Oct 3, 2026',
     'branch' => 'Evolution CMS',
-    'full_appname' => 'Evolution CMS 3.5.8 (Sep 10, 2026)'
+    'full_appname' => 'Evolution CMS 3.5.8 (Oct 3, 2026)'
 ];
